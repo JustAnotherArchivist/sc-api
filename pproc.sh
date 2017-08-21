bc98dad1db18623607839dba97aef0d02c424652
JustAnotherArchivist <JustAnotherArchivist@users.noreply.github.com>
2017-08-21 12:52:27 +0200

    Downloadable original size extraction

--- pproc.sh
+++ pproc.sh
@@ -1,6 +1,13 @@
 # Extract duration, number of tracks, and total original upload size from WARCs
 for n in 0000{1..7}; do zgrep -Po 'duration":\d+,' soundcloud-api-tracks-${n}.warc.gz | sed 's/[^0-9]//g' | awk '{ sum += $0; n += 1 } END { printf "Total duration: %.0f\nNumber of tracks: %d\n", sum, n }' > pproc-out/${n}; zgrep -Po 'original_content_size":\d+,' soundcloud-api-tracks-${n}.warc.gz | sed 's/[^0-9]//g' | awk '{ sum += $0 } END { printf "Total original size: %.0f\n", sum }' >> pproc-out/${n}; done
 
+# Extract list of 'trackId originalSize downloadable' from WARCs
+for i in 000{00..18}; do echo $(date) ${i}; zgrep -Po '((?<="kind":"track","id":)[^,]+|(?<=original_content_size":)[^,]+|(?<=downloadable":)[^,]+)(?=,)' soundcloud-api-tracks-${i}.warc.gz | awk '(NR % 3) == 2 { size=$0 } (NR % 3) == 1 { trackid=$0 } (NR % 3) == 0 { printf trackid " " size " " $0 "\n" }' > pproc-out/${i}.dlable; done; date
+
+# Post process those lists to get statistics
+for f in pproc-out/*.dlable; do echo $f; awk '{ dlable[$3]++; dlablesize[$3] += $2 } END { printf "downloadable: %.0f tracks / %.0f bytes\n", dlable["true"], dlablesize["true"]; printf "not downloadable: %.0f tracks / %.0f bytes\n", dlable["false"], dlablesize["false"] }' $f; echo; done
+awk '{ dlable[$3]++; dlablesize[$3] += $2 } END { printf "downloadable: %.0f tracks / %.0f bytes\n", dlable["true"], dlablesize["true"]; printf "not downloadable: %.0f tracks / %.0f bytes\n", dlable["false"], dlablesize["false"] }' pproc-out/*.dlable
+
 # tohours DURATION_IN_MS -> hours
 function tohours { echo "$(echo "scale=1; ${1} / 3600000" | bc) h"; }
 
